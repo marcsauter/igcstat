6c225362b00d5dc25f34b849dc81b0849641bf0b
Marc Sauter <marc.sauter@bluewin.ch>
2016-01-12 22:49:14 +0100

    Excel-Output, igc-Filename in output

--- Makefile
+++ Makefile
@@ -1,4 +1,8 @@
-build:
+build: clean
 	GOOS=linux GOARCH=amd64 go build -o igcstat_lnx
 	GOOS=darwin GOARCH=amd64 go build -o igcstat_osx
 	GOOS=windows GOARCH=amd64 go build -o igcstat.exe
+
+clean:
+	rm -f igcstat_lnx igcstat_osx igcstat.exe
+	rm -f *.csv *.xlsx
--- main.go
+++ main.go
@@ -13,23 +13,33 @@ import (
 	"github.com/marcsauter/flightstat"
 	"github.com/marcsauter/igc"
 	"github.com/marcsauter/wpt"
+	"github.com/tealeg/xlsx"
 )
 
 var (
-	takeoffsites, landingsites, outfile string
-	distance                            int
-	flights                             igc.Flights
+	takeoffsites, landingsites string
+	csvFile, xlsxFile          string
+	writeCsv, writeXlsx        bool
+	distance                   int
+	flights                    igc.Flights
 )
 
 func init() {
 	flag.StringVar(&takeoffsites, "takeoff", "Waypoints_Startplatz.gpx", "takeoff sites")
 	flag.StringVar(&landingsites, "landing", "Waypoints_Landeplatz.gpx", "landing sites")
 	flag.IntVar(&distance, "distance", 300, "maximal distance to the nearest known site")
-	flag.StringVar(&outfile, "outfile", fmt.Sprintf("%s.csv", filepath.Base(os.Args[0])), "output file")
+	flag.BoolVar(&writeCsv, "csv", false, "write csv output")
+	flag.StringVar(&csvFile, "csvfile", fmt.Sprintf("%s.csv", filepath.Base(os.Args[0])), "output file")
+	flag.BoolVar(&writeXlsx, "xlsx", true, "write xlsx output")
+	flag.StringVar(&xlsxFile, "xlsxfile", fmt.Sprintf("%s.xlsx", filepath.Base(os.Args[0])), "output file")
 }
 
 func main() {
 	flag.Parse()
+	if len(flag.Args()) != 1 {
+		flag.Usage()
+		os.Exit(0)
+	}
 	takeoff, err := wpt.NewWaypoints(takeoffsites)
 	if err == nil {
 		igc.RegisterTakeoffSiteSource(takeoff)
@@ -45,43 +55,77 @@ func main() {
 		log.Print(err)
 	}
 	sort.Sort(flights)
-	stat := flightstat.NewFlightStat()
-	out, err := os.Create(outfile)
+	stat, err := flightstat.NewFlightStat(&flights)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer out.Close()
-	w := csv.NewWriter(out)
-	for _, f := range flights {
-		if err := stat.Add(f); err != nil {
-			log.Fatal(err)
-		}
-		if err := w.Write(f.Record()); err != nil {
+	data := append(*flights.Output(), *stat.Output()...)
+	if writeCsv {
+		if err := writeCsvOutput(csvFile, &data); err != nil {
 			log.Fatal(err)
 		}
 	}
-	for _, s := range stat.Output() {
-		if err := w.Write(s); err != nil {
+	if writeXlsx {
+		if err := writeXlsxOutput(xlsxFile, &data); err != nil {
 			log.Fatal(err)
 		}
 	}
-	w.Flush()
 }
 
 func evaluate(path string, f os.FileInfo, err error) error {
-	if f.IsDir() {
+	if f.IsDir() || !strings.HasSuffix(f.Name(), ".igc") {
 		return nil
 	}
-	if strings.HasSuffix(f.Name(), ".igc") {
-		f, err := os.Open(path)
-		if err != nil {
+	flight, err := igc.NewFlight(path)
+	if err != nil {
+		return err
+	}
+	flights = append(flights, flight)
+	return nil
+}
+
+func writeCsvOutput(outfile string, data *[][]string) error {
+	out, err := os.Create(outfile)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+	w := csv.NewWriter(out)
+	//for _, f := range flights {
+	//if err := w.Write(f.Record()); err != nil {
+	//log.Fatal(err)
+	//}
+	//}
+	for _, s := range *data {
+		if err := w.Write(s); err != nil {
 			return err
 		}
-		flight, err := igc.NewFlight(f)
-		if err != nil {
-			return err
+	}
+	w.Flush()
+	return nil
+}
+
+func writeXlsxOutput(outfile string, data *[][]string) error {
+	//out, err := os.Create(outfile)
+	//if err != nil {
+	//return err
+	//}
+	//defer out.Close()
+	file := xlsx.NewFile()
+	sheet, err := file.AddSheet("Filght Statistics")
+	if err != nil {
+		return err
+	}
+	for _, r := range *data {
+		row := sheet.AddRow()
+		for _, c := range r {
+			cell := row.AddCell()
+			cell.Value = c
 		}
-		flights = append(flights, flight)
+	}
+	err = file.Save(outfile)
+	if err != nil {
+		return err
 	}
 	return nil
 }
