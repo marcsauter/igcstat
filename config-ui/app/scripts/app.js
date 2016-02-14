9595fd7a729dfc3393736dd0bbc9ccf0c3b999f1
Marc Sauter <marc.sauter@bluewin.ch>
2016-02-14 19:01:40 +0100

    yo angular

---
+++ config-ui/app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name igcstatConfigUiApp
+ * @description
+ * # igcstatConfigUiApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('igcstatConfigUiApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ config-ui/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name igcstatConfigUiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the igcstatConfigUiApp
+ */
+angular.module('igcstatConfigUiApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
